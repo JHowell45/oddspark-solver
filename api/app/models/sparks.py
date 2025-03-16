4af9bad33c9579cad6bc70f92993305c89301b92
= <jhowell45@googlemail.com>
2025-03-16 20:23:03 +0000

    adding model files.

---
+++ api/app/models/sparks.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field, SQLModel
+
+
+class BaseSpark(SQLModel):
+    name: str
+    defence: int = Field(min_length=0, max_length=5)
+
+
+class Spark(BaseSpark, table=True):
+    id: int | None = Field(default=None, primary_key=True)
