714cff8b91455103c0a8379cab04beec54ddd2c9
= <jhowell45@googlemail.com>
2025-03-16 20:53:33 +0000

    make sure the URi is a str for the engine.

--- api/app/core/db.py
+++ api/app/core/db.py
@@ -1,11 +1,15 @@
-from typing import Generator
+from typing import Annotated, Generator
 
 from app.core.config import get_settings
+from fastapi import Depends
 from sqlmodel import Session, create_engine
 
-engine = create_engine(get_settings().DATABASE_URI, echo=False)
+engine = create_engine(str(get_settings().DATABASE_URI), echo=False)
 
 
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
