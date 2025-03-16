fee7ae370eaa9944004f9818bd26ce0972849012
= <jhowell45@googlemail.com>
2025-03-16 21:26:39 +0000

    creating routes for the attack types.

--- api/app/routes/v1/sparks.py
+++ api/app/routes/v1/sparks.py
@@ -1,14 +1,23 @@
 from typing import Annotated
 
 from app.core.db import SessionDep
-from app.models.sparks import Spark, SparkCreate, SparkPublic
+from app.models.sparks import (
+    AttackType,
+    AttackTypePublic,
+    Spark,
+    SparkCreate,
+    SparkPublic,
+)
 from fastapi import APIRouter, Query
 from sqlmodel import select
 
 router = APIRouter(prefix="/sparks")
+attack_type_router = APIRouter(prefix="/attack-type")
+
+router.include_router(attack_type_router)
 
 
-@router.get("/")
+@router.get("/", response_model=list[SparkPublic])
 def get_sparks(
     session: SessionDep,
     offset: int = 0,
@@ -23,3 +32,12 @@ def create_spark(model: SparkCreate, session: SessionDep):
     session.commit()
     session.refresh(model)
     return model
+
+
+@attack_type_router.get("/", response_model=list[AttackTypePublic])
+def get_attack_types(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[AttackTypePublic]:
+    return session.exec(select(AttackType).offset(offset).limit(limit)).all()
