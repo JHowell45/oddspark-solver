561026cb28acead3fdcc660b9aaa709212da1401
= <jhowell45@googlemail.com>
2025-03-16 20:24:05 +0000

    fixing env for alembic for creating the migrations

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -1,13 +1,16 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from app.core.config import get_settings
+
+settings = get_settings()
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URI))
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +21,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.sparks import Spark
+
+target_metadata = Spark.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
